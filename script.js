e62d6b0c35c413fb7c8bea51679a6e27288a7d39
spg-AlexFuchs <fuc210322@spengergasse.at>
2025-11-14 12:55:30 +0100

    halb

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const prisma = new PrismaClient();
 const { fakerDE } = require('@faker-js/faker');
 
 
+function fahrradspeichern(){
+    const name = document.getElementById("bikename").value;
+    const farbe = document.getElementById("bikecolor").value;
+    const zoll = document.getElementById("bikesize").value;
+    const preis = document.getElementById("bikeprice").value;
+
+    const rad ={
+        name: name,
+        preis: preis,
+        zoll: zoll,
+        farbe: farbe
+        }
+    console.log(rad);
+    await prisma.Fahrrad.create({data: rad});
+}
+
+
+
+
 async function dateneingabe(string name, float preis, float zoll, string farbe){
     const prisma = new PrismaClient()
     
